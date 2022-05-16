6c327cdcb3110f630982d65034569fc5706a09a9
kuhmasii <olaisaiah54@gmail.com>
2022-05-16 17:49:55 +0100

    refactor: changed the routing to the path function (this will be easier for newbies that want to use the source code in learning, because they will be downloading the latest version of django)

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 app_name = 'orders'
 
 urlpatterns = [
-    url(r'^create/$', views.order_create, name='order_create'),
-    url(r'^webhook$', views.webhook, name='webhook')
+    path('', views.order_create, name='order_create'),
+    path('', views.webhook, name='webhook')
 ]
